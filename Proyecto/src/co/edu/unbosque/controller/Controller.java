46d6c103c977bc1d6c616814c5844f9c91457e80
Juan Sebastian Muñoz <SebastianM@Laptop>
2022-02-10 21:03:53 -0500

    Creacion de paquetes MVC,Apl Main y View.

---
+++ Proyecto/src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,26 @@
+package co.edu.unbosque.controller;
+
+
+import co.edu.unbosque.view.View;
+
+public class Controller {
+
+	
+	private View gui;
+	
+	public Controller() {
+		
+	
+		gui = new View();
+		
+		funcionar();
+		
+	}
+	
+	public void funcionar() {
+		
+
+		
+	}
+	
+}
